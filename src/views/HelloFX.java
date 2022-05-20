20c3a549dca7a38edacdb9df68698bd3c397e05b
Marek <59203712+MarekPrim@users.noreply.github.com>
2022-05-20 09:13:34 +0200

    test

--- src/views/HelloFX.java
+++ src/views/HelloFX.java
@@ -1,22 +1,12 @@
 package views;
 
-import java.lang.ModuleLayer.Controller;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.Label;
-import javafx.scene.control.TitledPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
-
-	private static final int Infinity = 0;
 
 	@Override
     public void start(Stage stage) throws Exception {
