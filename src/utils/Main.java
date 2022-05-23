8d201374a347b5732c048f8b17959033c355d6c2
Marek <59203712+MarekPrim@users.noreply.github.com>
2022-05-23 11:52:42 +0200

    wip récupération et parsing ics

--- src/utils/Main.java
+++ src/utils/Main.java
@@ -1,8 +1,8 @@
 package utils;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+
 
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
@@ -10,11 +10,21 @@ import net.fortuna.ical4j.model.Calendar;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, ParserException {
-		FileInputStream fin = new FileInputStream("/home/greenteam/N7/S6/CPO/projet-long/Joodle/src/utils/ADECal.ics");
+	public static void main(String[] args) throws IOException {
+		//RequestFormeur rq = new RequestFormeur("2022-07-02","2022-07-15");
+		//rq.write();
+		
+		FileInputStream fin = new FileInputStream("ADECal.ics");
 		CalendarBuilder builder = new CalendarBuilder();
-		Calendar calendar = builder.build(fin);
-
+		try {
+			Calendar calendar = builder.build(fin);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/utils/RequestFormeur.java
+++ src/utils/RequestFormeur.java
@@ -1,6 +1,8 @@
 package utils;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -12,25 +14,32 @@ import java.nio.file.StandardCopyOption;
 
 public class RequestFormeur {
 	
-	public static final String url = "https://edt.inp-toulouse.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=3579&projectId=35&calType=ical&";
+	public static final String url = "https://edt.inp-toulouse.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=3730&projectId=35&calType=ical&";
 	//https://edt.inp-toulouse.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=3579&projectId=35&calType=ical&firstDate=2021-08-01&lastDate=2022-07-15
 	
 	private URL urlRequest;
 	
 	public RequestFormeur(String start,String end) throws IOException {
-		this.urlRequest = new URL(RequestFormeur.url+"firstDate="+start+"&lastDate"+end);
+		this.urlRequest = new URL("https://edt.inp-toulouse.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=3730&projectId=35&calType=ical&firstDate=2021-08-01&lastDate=2022-07-15");
 	}
 	
 	public void write() throws IOException {
-		InputStream in = this.urlRequest.openStream();
-		File targetFile = new File("/home/greenteam/ici.ics");
-
-	    Files.copy(
-	      in, 
-	      targetFile.toPath(), 
-	      StandardCopyOption.REPLACE_EXISTING);
-	    in.close();
+		Path addresseFile = Files.createTempFile("calendrier", ".ics");
+        BufferedInputStream inputStream = new BufferedInputStream(this.urlRequest.openStream());
+        
+        while (inputStream.available() > 0) {
+              
+                // Read the byte and
+                // convert the integer to character
+                char c = (char)inputStream.read();
+      
+                // Print the characters
+                
+       }
+        Files.copy(inputStream, addresseFile, StandardCopyOption.REPLACE_EXISTING);
 	}
+	
+
 	
 	
 
