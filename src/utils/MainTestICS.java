8206327200b9570af7b09c273b23ce5126b89565
Yoan Prosdocimi <yoanprosdocimi@gmail.com>
2022-05-24 09:03:11 +0200

    Fix import

--- src/utils/MainTestICS.java
+++ src/utils/MainTestICS.java
@@ -1,12 +1,7 @@
 package utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.ParseException;
-
-import net.fortuna.ical4j.data.CalendarBuilder;
-import net.fortuna.ical4j.data.ParserException;
-import net.fortuna.ical4j.model.Calendar;
 
 public class MainTestICS {
 
