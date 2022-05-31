e187d32c7d0285b2e1bb2a451a83873261e7349c
Armandcollab <armand.loris.claveau@gmail.com>
2022-05-31 17:15:10 +0200

    premisses controller

---
+++ src/controllers/ControllerResteauU.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+public class ControllerResteauU implements Initializable{
+
+	@FXML private TextField Jour1Menu;
+	@FXML private TextField Jour1Date;
+	
+	@FXML private TextField Jour2Menu;
+	@FXML private TextField Jour2Date;
+	
+	@FXML private TextField Jour3Menu;
+	@FXML private TextField Jour3Date;
+	
+	@FXML private TextField Jour4Menu;
+	@FXML private TextField Jour4Date;
+	
+	@FXML private TextField Jour5Menu;
+	@FXML private TextField Jour5Date;
+	
+	
+	
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+   
+    	//TODO charge le menu 
+    	// le parse
+    	// et tada
+    }
+	
+}
---
+++ src/modele/menu/Repa.java
@@ -0,0 +1,18 @@
+package modele.menu;
+
+import java.util.List;
+
+public class Repa {
+	
+	private String date;
+	private List<String> plats;
+	
+	public Repa(String date, List<String> plats) {
+		super();
+		this.date = date;
+		this.plats = plats;
+	}
+	
+	
+
+}
