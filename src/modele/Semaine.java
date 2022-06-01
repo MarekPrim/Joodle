e0a5391b5fdb023d5d2fa38a5499224060bb872d
Eliott <eliott.duclos@gmail.com>
2022-06-01 15:19:38 +0200

    Ajout modèle Semaine

---
+++ src/modele/Semaine.java
@@ -0,0 +1,31 @@
+package modele;
+
+/**
+ * Regroupe les informations d'un �tudiant qui sont utilis�es par l'application
+ * Un seul profil d'�tudiant n'est disponible par application, ce profil est acc�sible avec la m�thode getInstanceEtudiant.
+ */
+
+public class Semaine {
+
+	/**
+	 * Permet la s�rialisation et d�s�rialisation des donn�es pour les sauvegarder dans un fichier.
+	 * https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/Serializable.html
+	 */
+	private static final long serialVersionUID = -2882072551330105119L;
+
+	private date week;
+	private date monday_week;
+
+	private Semaine() {
+		this.week = ;
+    this.monday_week = ;
+	}
+
+  public void increase {
+    echo increase;
+  }
+
+  public void decrease {
+    echo decrease;
+  }
+}
