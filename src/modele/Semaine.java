7ab3db6145d4bd7f1159f0984ddfa07eb4d71ea8
Eliott <eliott.duclos@gmail.com>
2022-06-01 15:32:20 +0200

    Changement nue profil étudiant

--- src/modele/Semaine.java
+++ src/modele/Semaine.java
@@ -1,17 +1,6 @@
 package modele;
 
-/**
- * Regroupe les informations d'un �tudiant qui sont utilis�es par l'application
- * Un seul profil d'�tudiant n'est disponible par application, ce profil est acc�sible avec la m�thode getInstanceEtudiant.
- */
-
 public class Semaine {
-
-	/**
-	 * Permet la s�rialisation et d�s�rialisation des donn�es pour les sauvegarder dans un fichier.
-	 * https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/Serializable.html
-	 */
-	private static final long serialVersionUID = -2882072551330105119L;
 
 	private date week;
 	private date monday_week;
