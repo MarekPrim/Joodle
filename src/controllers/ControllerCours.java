c1821fb35100297d034764e15de60543e30b4de7
Marek <59203712+MarekPrim@users.noreply.github.com>
2022-06-03 17:59:41 +0200

    web view moodle

--- src/controllers/ControllerCours.java
+++ src/controllers/ControllerCours.java
@@ -1,5 +1,20 @@
 package controllers;
 
-public class ControllerCours {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebView;
+
+public class ControllerCours implements Initializable{
+
+    @FXML
+    private WebView webMoodle;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		webMoodle.getEngine().load("https://moodle-n7.inp-toulouse.fr/");
+	}
 
 }
