dd84f57695dec1cdc16cf47a8e49dc55f63a24a7
Yoan Prosdocimi <yoanprosdocimi@gmail.com>
2022-06-07 17:31:55 +0200

    User story #7 : Ajout des boutons pour ajouter une ue et un fichier.

--- src/controllers/ControllerNotes.java
+++ src/controllers/ControllerNotes.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import utils.Utils;
 
 public class ControllerNotes {
@@ -20,23 +21,44 @@ public class ControllerNotes {
 	private TextArea editeur_text;
 	
 	@FXML
-	private Button bouton_valider;
+	private Button bouton_ajouter_fichier;
+	
+	@FXML
+	private Button bouton_ajouter_dossier;
+	
+	@FXML
+	private TextField texte_ajouter_fichier;
+	
+	@FXML
+	private TextField texte_ajouter_dossier;
 	
 	private String nomFichierModifie = "test";
 	
-	private String nomCoursModifie = "UE_test";
+	private String nomCoursModifie = "UE_Test";
 
 	
 	@FXML
 	private void enregistrerFichier(ActionEvent e) throws IOException {
-		String addresseDossierJoodle = Utils.addresseDossierDonneesApplication();
-		File fichier = new File(addresseDossierJoodle + File.separator + nomCoursModifie + File.separator
+		String addresseDossierDocument = Utils.addresseDossierDocumentJoodle();
+		File fichier = new File(addresseDossierDocument + File.separator + nomCoursModifie + File.separator
 				+ nomFichierModifie + ".txt");
 		
 		
         FileOutputStream fos = new FileOutputStream(fichier);
         fos.write(editeur_text.getText().getBytes());
         fos.close();
+	}
+	
+	@FXML
+	private void ajouterDossier(ActionEvent e) {
+		this.bouton_ajouter_dossier.setVisible(false);
+		this.texte_ajouter_dossier.setVisible(true);
+	}
+	
+	@FXML
+	private void ajouterFichier(ActionEvent e) {
+		this.bouton_ajouter_fichier.setVisible(false);
+		this.texte_ajouter_fichier.setVisible(true);
 	}
 	
 
