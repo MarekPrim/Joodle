efd3b82057fa68b319ea5ffbdfe21b6def0241a2
Yoan Prosdocimi <yoanprosdocimi@gmail.com>
2022-06-07 16:48:54 +0200

    User story : #7 : Enregistrement de notes.

--- src/controllers/ControllerNotes.java
+++ src/controllers/ControllerNotes.java
@@ -1,23 +1,43 @@
 package controllers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import utils.Utils;
 
 public class ControllerNotes {
 	
 	@FXML
-	private TextArea edition_text;
+	private TextArea editeur_text;
 	
 	@FXML
 	private Button bouton_valider;
 	
-	private String nomCoursModifie = "test";
+	private String nomFichierModifie = "test";
 	
-	private String nomFichierModifie = "UE_test";
+	private String nomCoursModifie = "UE_test";
 
 	
-	
+	@FXML
+	private void enregistrerFichier(ActionEvent e) throws IOException {
+		String addresseDossierJoodle = Utils.addresseDossierDonneesApplication();
+		File fichier = new File(addresseDossierJoodle + File.separator + nomCoursModifie + File.separator
+				+ nomFichierModifie + ".txt");
+		
+		
+        FileOutputStream fos = new FileOutputStream(fichier);
+        fos.write(editeur_text.getText().getBytes());
+        fos.close();
+	}
 	
 
 }
