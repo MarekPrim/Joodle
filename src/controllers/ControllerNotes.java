323dd421c17e45ba12bb8e08692ef5db69518eae
Yoan Prosdocimi <yoanprosdocimi@gmail.com>
2022-06-07 16:39:55 +0200

    User story : #7 : notes - > resultats

--- src/controllers/ControllerNotes.java
+++ src/controllers/ControllerNotes.java
@@ -1,5 +1,23 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class ControllerNotes {
+	
+	@FXML
+	private TextArea edition_text;
+	
+	@FXML
+	private Button bouton_valider;
+	
+	private String nomCoursModifie = "test";
+	
+	private String nomFichierModifie = "UE_test";
+
+	
+	
+	
 
 }
