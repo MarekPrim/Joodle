3f93f5a18ac945b42ffa6cd0568b35d23121f12e
Yoan Prosdocimi <yoanprosdocimi@gmail.com>
2022-06-08 11:56:20 +0200

    User story #7 : Création des fichiers de notes

--- src/controllers/ControllerManageurFichierNotes.java
+++ src/controllers/ControllerManageurFichierNotes.java
@@ -1,14 +1,21 @@
 package controllers;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import modele.DossierNotes;
+import modele.FichierNotes;
 
 public class ControllerManageurFichierNotes implements Initializable{
 	
@@ -22,8 +29,38 @@ public class ControllerManageurFichierNotes implements Initializable{
 	private TitledPane panneauDossier;
 	
 	@FXML
+	private TextField texte_ajouter_fichier;
+	
+	@FXML
 	private void ajouterFichier(ActionEvent e) {
-		this.bouton_ajouter_fichier.setVisible(false);
+		if (this.texte_ajouter_fichier.isVisible()) {
+			if (!this.texte_ajouter_fichier.getText().isBlank()) {
+				enregistrementNouveauFichier();
+			}
+			else {
+				texte_ajouter_fichier.setVisible(false);
+			}
+		}
+		else {
+			this.texte_ajouter_fichier.setVisible(true);
+		}
+	}
+	
+
+	private void enregistrementNouveauFichier() {
+		File fichier = new File(dossier.getCheminDossier() + File.separator + "Notes_" + texte_ajouter_fichier.getText() + ".txt");
+		if(!fichier.exists()) {
+			try {
+				fichier.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			dossier.ajouterFichier(new FichierNotes(fichier.getPath(), texte_ajouter_fichier.getText()));
+		}
+		texte_ajouter_fichier.setText("");
+		texte_ajouter_fichier.setVisible(false);
+		
 	}
 	
 	public ControllerManageurFichierNotes(DossierNotes dossier) {
@@ -33,6 +70,22 @@ public class ControllerManageurFichierNotes implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		panneauDossier.setText(dossier.getNomDossier());
+		texte_ajouter_fichier.setOnKeyPressed(new EventHandlerEnregistrementFichier());
+	}
+	
+	private class EventHandlerEnregistrementFichier implements EventHandler<KeyEvent> {
+
+		@Override
+		public void handle(KeyEvent e) {
+			if (e.getCode().equals(KeyCode.ENTER)) {
+				if (!texte_ajouter_fichier.getText().isBlank()) {
+					enregistrementNouveauFichier();
+				}
+				else {
+					texte_ajouter_fichier.setVisible(false);
+				}
+			}
+		}
 		
 	}
 
